3f857449303a6b2f63065240abed0cb9574932ea
Eduardo Reveco <eduardorevecovillalobos@gmail.com>
2021-04-26 13:33:21 -0400

    varieties get

--- src/routes/api/varieties.routes.js
+++ src/routes/api/varieties.routes.js
@@ -3,6 +3,28 @@ import Variety from '../../models/Variety'
 export default [
     {
         method: 'GET',
+        path: '/api/varieties',
+        options: {
+            description: 'get all varieties',
+            notes: 'get all varieties',
+            tags: ['api'],
+            handler: async (request, h) => {
+                try {
+                    let findVarieties = await Variety.find({})
+
+                    return findVarieties
+                } catch (error) {
+                    console.log(error)
+
+                    return h.response({
+                        error: 'Internal Server Error'
+                    }).code(500)
+                }
+            }
+        }
+    },
+    {
+        method: 'GET',
         path: '/api/varieties/{productId}',
         options: {
             description: 'get varieties by productId',
